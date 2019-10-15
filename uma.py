446b3aba392c64cc251a3a8bbe649485e564af5e
umamaheshN <56362233+umamaheshN@users.noreply.github.com>
2019-10-15 22:55:21 +0530

    Initial commit

---
+++ uma.py
@@ -0,0 +1,11 @@
+Input_var = {
+    "http-request-ext": {
+        "request_header": {
+            "User-Agent": "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.1;)",
+            "user-Agent2" :"Chrome/6.0"
+        },
+        "request_method": "unknown",
+        "request_value": "unknown",
+    },
+    "request_header" :"data-not avaliable"
+}
